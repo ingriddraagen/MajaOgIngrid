38d1d97954e276ecad74e38ee1fd95cfcdb53ba6
Ingrid Gormsdatter Draagen <ingrid.draagen@gmail.com>
2019-01-16 13:26:31 +0100

    the topnav is now set up with js and jQuery

---
+++ amazingWebsite/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+// Top navigator:
+$( ".topnav" ).append( "  <a class=\"button\" href=\"thicc.html\"><img src=\"img/thiccDog.png\" alt=\"Thicc buttn\"></a>");
+$( ".topnav" ).append( "  <a class=\"button\" href=\"puppers.html\"><img src=\"img/puppers.png\" alt=\"puppers buttn\"></a> ");
+$( ".topnav" ).append( "   <a class=\"active\" href=\"index.html\"><img class=\"mainLogo\" src=\"img/GoodDoggo.png\" alt=\"Header pictur of a happy dog\"> </a> ");
+$( ".topnav" ).append( "   <a class=\"button\" href=\"floofs.html\"><img  src=\"img/floofs.png\" alt=\"floof buttn\"></a> ");
+$( ".topnav" ).append( "   <a class=\"button\" href=\"happee.html\"><img src=\"img/happeeDog.png\" alt=\"happee buttn\"></a> ");
+
+
+
+});
